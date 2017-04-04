b5a70fc60ff15ba7d18f576e5151ddb15a5db87d
RonanGui <ronan.guillaume@etu.univ-tours.fr>
2017-04-04 21:41:18 +0200

    Class VirtualDptTest

---
+++ test/model/VirtualDepartmentTest.java
@@ -0,0 +1,32 @@
+package model;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created by Ronan
+ * on 04/04/2017.
+ */
+public class VirtualDepartmentTest {
+
+    @BeforeClass
+    static public void setUpBeforeClass() throws Exception {
+
+    }
+
+    @Test
+    public void getNbEmployees() throws Exception {
+
+    }
+
+    @Test
+    public void getEmployeeById() throws Exception {
+
+    }
+
+    @Test
+    public void addEmployee() throws Exception {
+
+    }
+
+}
