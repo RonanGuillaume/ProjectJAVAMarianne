0a2f634798f9de8ce9399e94853050206a884deb
RonanGui <ronan.guillaume@etu.univ-tours.fr>
2017-04-04 20:39:39 +0200

    Class Manager

--- src/model/Manager.java
+++ src/model/Manager.java
@@ -1,8 +1,14 @@
 package model;
 
+import java.util.Date;
+
 /**
  * Created by Ronan
  * on 04/04/2017.
  */
-public class Manager {
+public class Manager extends Employee {
+
+    public Manager(String name, String firstName, Date startHour, Date endHour, Date creditHour) {
+        super(name, firstName, startHour, endHour, creditHour);
+    }
 }
