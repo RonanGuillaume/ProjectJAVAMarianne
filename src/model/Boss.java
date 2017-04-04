c658d5a8e7cbd7722653bee633580e91fc2fc189
RonanGui <ronan.guillaume@etu.univ-tours.fr>
2017-04-04 20:28:05 +0200

    Class person and boss

--- src/model/Boss.java
+++ src/model/Boss.java
@@ -4,5 +4,9 @@ package model;
  * Created by Ronan
  * on 04/04/2017.
  */
-public class Boss {
+public class Boss extends Person{
+
+    public Boss(String name, String firstName) {
+        super(name, firstName);
+    }
 }
--- src/model/Person.java
+++ src/model/Person.java
@@ -5,4 +5,27 @@ package model;
  * on 04/04/2017.
  */
 public class Person {
+    private String name;
+    private String firstName;
+
+    public Person(String name, String firstName) {
+        this.name = name;
+        this.firstName = firstName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
